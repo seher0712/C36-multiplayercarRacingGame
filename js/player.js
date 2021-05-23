b761494b4de259571801afd22ab8f49e550c1f46
seher0712 <75379142+seher0712@users.noreply.github.com>
2021-05-23 17:03:42 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,20 @@
+class Player{
+    constructer(){}
+    getCount(){
+        var playerCountRef = database.ref('playerCount')
+        playerCountRef.on("value",function(data){
+            playerCount = data.val()
+        })
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount:count
+        })
+    }
+    update(name){
+        var playerIndex="player"+playerCount
+        database.ref(playerIndex).set({
+            name:name
+        })
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas, backgroundImage, database
+var gameState=0
+var playerCount
+var form , player, game
+
+function setup(){
+canvas = createCanvas(400,400)
+database = firebase.database()
+game = new Game()
+  game.getState()
+game.start()
+
+}
+
+function draw(){
+
+}
